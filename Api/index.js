c065ad890ed82936a5cee60a3c95297b9c6c821a
Sarah <ssarashahabii@gmail.com>
2024-06-22 13:00:08 +0330

    Add preproduction changes

--- Api/index.js
+++ Api/index.js
@@ -5,14 +5,23 @@ import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import responseMiddleware from "./middlewares/responseMiddlewares.js";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 dotenv.config();
 
 const app = express();
 
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+
 app.use(responseMiddleware);
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 try {
     await mongoose.connect(process.env.MONGO_URL);
@@ -26,7 +35,6 @@ try {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
